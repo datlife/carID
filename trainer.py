883df1d5c3a6bbca057e19b24b4e54a46dff72e0
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-03-01 15:44:24 -0800

    Updated trainer

--- trainer.py
+++ trainer.py
@@ -17,44 +17,19 @@ tf.logging.set_verbosity(tf.logging.DEBUG)
 # @TODO: config augmentation during training/testing
 
 
-def parse_args():
-  """Command line arguments.
-  """
-  parser = argparse.ArgumentParser(
-    description="Train a Car Re-identification model")
-
-  parser.add_argument(
-    "--dataset_dir", help="Path to dataset directory.",
-    default=None, required=True)
-
-  parser.add_argument(
-    "--steps", help="Number of training steps",
-    default=1, type=int)
-  parser.add_argument(
-    "--batch_size", help="Number of training instances for every iteration",
-    default=32, type=int)
-
-  parser.add_argument(
-    "--model_dir", help="Path to store log and trained model",
-    default=None)
-  return parser.parse_args()
-
-
 def train():
   args = parse_args()
-
   veri_dataset = VeRiDataset(root_dir=args.dataset_dir).load()
 
+  # Load model
   model = resnet50()
   model.compile(
     optimizer='adam',
     loss=triplet_loss(margin=0.2),
     metrics=[ap_distance, an_distance])
-
   model.summary()
 
   # Train / Logging
-
   train_input_fn, val_input_fn = veri_dataset.get_input_fn(
       is_training=True,
       batch_size=args.batch_size,
@@ -86,14 +61,36 @@ def keras_callbacks(logdir):
     tf.keras.callbacks.TensorBoard(
       log_dir=logdir,
       write_graph=False),
-
     tf.keras.callbacks.ModelCheckpoint(
       filepath=os.path.join(logdir, 'carid.weights'),
       monitor='val_loss',
       save_best_only=True,
-      save_weights_only=True)
-  ]
+      save_weights_only=True)]
+
   return callbacks
+
+
+def parse_args():
+  """Command line arguments.
+  """
+  parser = argparse.ArgumentParser(
+    description="Train a Car Re-identification model")
+
+  parser.add_argument(
+    "--dataset_dir", help="Path to dataset directory.",
+    default=None, required=True)
+
+  parser.add_argument(
+    "--steps", help="Number of training steps",
+    default=1, type=int)
+  parser.add_argument(
+    "--batch_size", help="Number of training instances for every iteration",
+    default=32, type=int)
+
+  parser.add_argument(
+    "--model_dir", help="Path to store log and trained model",
+    default=None)
+  return parser.parse_args()
 
 
 if __name__ == '__main__':
--- visualize_tnse.py
+++ visualize_tnse.py
@@ -9,9 +9,44 @@ from __future__ import print_function
 import os
 import argparse
 import keras
+import tensorflow as tf
+from tensorboard.plugins.projector import projector_plugin
 
 from carid.models import resnet50
 from carid.dataset import VeRiDataset
+
+
+def construct_projector_summary(args):
+  config = projector_plugin.ProjectorConfig()
+  embed = config.embeddings.add()
+  embed.tensor_name = 'embedding:0'
+  embed.metadata_path = os.path.join(
+    args.logdir, 'projector/metadata.tsv')
+  embed.sprite.image_path = os.path.join(
+    args.model_dir, 'veri_sprite_10k.png')
+  embed.sprite.single_image_dim.extend([28, 28])
+
+
+def visualize():
+  args = parse_arguments()
+
+  # Load data
+  veri_data = VeRiDataset(root_dir=args.data_dir).load()
+
+  # Load model
+  inputs = tf.keras.layers.Input(shape=(224, 224, 3))
+  model = keras.models.Model(
+    inputs=inputs,
+    outputs=tf.keras.layers.Lambda(
+      lambda x: x, "embedding")(resnet50()(inputs)))
+  model.compile('sgd', 'mse')
+  model.summary()
+
+  # Get embeddings
+  samples = veri_data.get_samples(num_samples=50, num_classes=10)
+
+
+  return 0
 
 
 def parse_arguments():
@@ -30,27 +65,6 @@ def parse_arguments():
     type=int, default=50)
 
   return parser.parse_args()
-
-
-def visualize():
-
-  args = parse_arguments()
-
-  # Load data
-  veri_data = VeRiDataset(root_dir=args.data_dir).load()
-  samples = veri_data.get_samples(num_samples=50, num_classes=10)
-
-  # Define model
-  model = resnet50().compile('sgd', 'mse')
-
-  # Visualize
-  callbacks = keras.callbacks.TensorBoard(
-    log_dir=args.logdir,
-    write_graph=False, embeddings_freq=1
-  )
-
-
-  return 0
 
 
 if __name__ == '__main__':
