6e8817e2b39cda6fbca6f08e776370b8337a6e0a
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-02-28 19:56:04 -0800

    Working

--- carid/dataset.py
+++ carid/dataset.py
@@ -3,42 +3,55 @@ r"""Dataset loader for Triplet Model
 """
 import os
 import cv2
+import random
 import pandas as pd
+
 from lxml import etree
 import tensorflow as tf
-
-
-class DataLoader(object):
-  """Base DataLoader
-
-  """
+import multiprocessing as mp
+from sklearn.model_selection import train_test_split
 
 
 class VeRiDataset(object):
   """VeRi Dataset Loader
 
   This dataset is for training vehicle re-identification model
+
+  Default attributes
+
+  FIELDS - a list of headers
+  TRAIN_XML - training label file
+  TRAIN_DIR - path to images for training
+
   """
+  TRAIN_XML = 'train_label.xml'
+  TRAIN_DIR = 'image_train'
   FIELDS = ['vehicleID', 'imageName', 'typeID', 'cameraID']
 
-  def __init__(self, path="VeRi"):
-    self.path = path
+  def __init__(self, root_dir):
     self.data = None
+    self.root_dir = root_dir
 
   def load(self):
-    if not os.path.isfile(self.path):
-      raise IOError("Cannot load %s" % self.path)
-    with open(self.path, 'rb') as fio:
-      xml_stream = fio.read()
+    """Parse XML and load data into `self.data`
+    """
+    xml_file = os.path.join(self.root_dir, VeRiDataset.TRAIN_XML)
 
+    if not os.path.isfile(xml_file):
+      raise IOError("Cannot load %s" % path)
+
+    with open(xml_file, 'rb') as fio:
+      xml_stream = fio.read()
     et = etree.fromstring(xml_stream)
+
     items = et.xpath('Items/Item')
     self.data = pd.DataFrame(
       [dict(item.attrib) for item in items],
       columns=VeRiDataset.FIELDS)
+
     return self
 
-  def get_input_fn(self, is_training, batch_size, shuffle):
+  def get_input_fn(self, is_training, batch_size, shuffle, buffer_size=1000):
     """Create a input function
 
     Args:
@@ -53,14 +66,95 @@ class VeRiDataset(object):
     if self.data is None:
       raise ValueError("Data is currently empty. Did you call load()?")
 
-    dataset = tf.data.Dataset.from_tensor_slices(
-      ({'input_anchor': None,
-        'input_positive': None,
-        'input_negative': None},
-       [0.0, 0.0, 0.0])
-    )
+    training, validation = train_test_split(self.data, test_size=0.3)
+    # @TODO : create Transformer
 
-    dataset = dataset.shuffle(1000)
-    dataset = dataset.make_one_shot_iterator().get_next()
+    training_data = tf.data.Dataset.from_generator(
+      lambda: self.generator(
+        data_frames=training,
+        is_training=is_training),
+      3 * (tf.float32,),
+      3 * (tf.TensorShape([None, None, 3]),))
 
-    return dataset
+    training_data = (training_data.prefetch(buffer_size).
+              map(self._preprocess(is_training),
+                  num_parallel_calls=mp.cpu_count()).
+              prefetch(buffer_size).
+              shuffle(buffer_size).
+              batch(batch_size).
+              make_one_shot_iterator().
+              get_next())
+
+    validataion_data = tf.data.Dataset.from_generator(
+      lambda: self.generator(
+        data_frames=validation,
+        is_training=False),
+      3 * (tf.float32,),
+      3 * (tf.TensorShape([None, None, 3]),))
+
+    validataion_data = (validataion_data.prefetch(buffer_size).
+              map(self._preprocess(is_training),
+                  num_parallel_calls=mp.cpu_count()).
+              prefetch(buffer_size).
+              batch(batch_size).
+              make_one_shot_iterator().
+              get_next())
+
+    return training_data, validataion_data
+
+  def _preprocess(self, is_training):
+    """Preprocessor for VeriDataset
+
+    Args:
+      is_training:
+
+    Returns:
+
+    """
+    def convert_to_dict(anchor, positive, negative):
+      return ({
+        'input_anchor':   anchor,
+        'input_positive': positive,
+        'input_negative': negative,
+      }, tf.random_normal([1, 1, 3]))
+
+    return convert_to_dict
+
+  def generator(self,data_frames, is_training):
+    """Generate a triple instances.
+
+    Given a list of instances with different object IDs,
+    this method would generate a triplet (anchor, positive, negative)
+    instances for every iteration such that:
+
+    * Anchor and positive instances have the same Object ID.
+    * Negative instance have a object_id different than anchor, positive
+
+    Args:
+      data_frames - pandas.DataFrame
+      is_training -  Boolean
+
+
+    Returns:
+      generator - a data generator.
+
+    """
+
+    groups = data_frames.groupby('vehicleID')
+    group_names = groups.groups.keys()
+
+    data_dir = os.path.join(self.root_dir,VeRiDataset.TRAIN_DIR)
+    while True:
+      ids = random.sample(group_names, 2)
+      anchor, positive = groups.get_group(ids[0]).sample(2).to_dict('records')
+      negative = groups.get_group(ids[1]).sample(1).to_dict('records')[0]
+      anchor, positive, negative = [
+        cv2.resize(
+          cv2.imread(
+            os.path.join(data_dir, sample['imageName']),
+            cv2.IMREAD_COLOR),
+          (224, 224))
+        for sample in [anchor, positive, negative]
+      ]
+
+      yield anchor, positive, negative
--- trainer.py
+++ trainer.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
 import argparse
 import numpy as np
 import tensorflow as tf
@@ -11,65 +12,66 @@ import tensorflow as tf
 from carid.losses import triplet_loss
 from carid.dataset import VeRiDataset
 
+tf.logging.set_verbosity(tf.logging.DEBUG)
+# @TODO: config augmentation during training/testing
+
 
 def parse_args():
-  """ Parse command line arguments.
-
-  Returns
-    args - arguments object
+  """Command line arguments.
   """
+
   parser = argparse.ArgumentParser(
     description="Train a Car Re-identification model")
   parser.add_argument(
-    "--training_labels", help="Path to training XML files.",
+    "--dataset_dir", help="Path to dataset directory.",
     default=None, required=True)
+
+  parser.add_argument(
+    "--steps", help="Number of training steps",
+    default=1, type=int)
   parser.add_argument(
     "--batch_size", help="Number of training instances for every iteration",
-    default=32)
+    default=32, type=int)
+
   parser.add_argument(
     "--model_dir", help="Path to store log and trained model",
     default=None)
   return parser.parse_args()
 
-
 def train():
   args = parse_args()
 
-  # ##################
-  # Define Dataset
-  # ##################
-  dataset = VeRiDataset(path=args.training_labels).load()
+  veri_dataset = VeRiDataset(root_dir=args.dataset_dir).load()
 
-  # ##################
-  # Define model
-  # ##################
-  inputs = [tf.keras.Input(
-    shape=(None, None, 3),
-    name="input_%s" % in_type)
-    for in_type in ['anchor', 'positive', 'negative']]
+  training_input_fn, validataion_input_fn = veri_dataset.get_input_fn(
+      is_training=True,
+      batch_size=args.batch_size,
+      shuffle=True)
 
-  resnet50 = tf.keras.applications.resnet50.ResNet50(
-    include_top=False,
-    input_shape=(None, None, 3))
+  model = build_model()
+  model.compile(optimizer='adam', loss=triplet_loss(margin=0.2))
 
-  model = tf.keras.Model(
-    inputs=inputs,
-    outputs=[resnet50(input) for input in inputs],
-    name="CarReId_net")
-
-  model.compile(
-    optimizer='adam',
-    loss=triplet_loss(margin=0.2))
-
-  estimator = tf.keras.estimator.model_to_estimator(
-    keras_model=model,
-    model_dir=args.model_dir)
-
-  # ##################
   # Train / Logging
-  # ##################
-  # @TODO: construct input_fn
-  # @TODO: config augmentation during training/testing
+  callbacks = [
+    tf.keras.callbacks.TensorBoard(
+      log_dir=args.model_dir,
+      write_graph=False),
+
+    tf.keras.callbacks.ModelCheckpoint(
+      filepath=os.path.join(args.model_dir,'{epoch:02d}-{val_loss:.2f}.hdf5'),
+      monitor='val_loss',
+      save_best_only=True
+    )]
+
+  model.fit_generator(
+    epochs=args.steps,
+    steps_per_epoch=100,
+    generator=keras_generator(input_fn=training_input_fn),
+    validation_data=keras_generator(input_fn=validataion_input_fn),
+    validation_steps=100,
+    callbacks=callbacks,
+    workers=0,
+    verbose=1)
 
   # ##################
   # Evaluate
@@ -78,6 +80,44 @@ def train():
   print("---- Training Completed ----")
   return 0
 
+def keras_generator(input_fn):
+  K = tf.keras.backend
+
+  while True:
+    yield K.get_session().run(input_fn)
+
+def build_model():
+  inputs = [tf.keras.Input(
+    shape=(224, 224, 3),
+    name="input_%s" % in_type)
+    for in_type in ['anchor', 'positive', 'negative']]
+
+  resnet50 = tf.keras.applications.resnet50.ResNet50(
+    include_top=False,
+    input_shape=(224, 224, 3))
+
+  outputs = tf.keras.layers.concatenate(
+    [resnet50(input) for input in inputs],
+    name='output')
+
+  model = tf.keras.Model(
+    inputs=inputs,
+    outputs=outputs,
+    name="CarReId_net")
+  return model
 
 if __name__ == '__main__':
   train()
+
+
+  # estimator = tf.keras.estimator.model_to_estimator(
+  #   keras_model=model,
+  #   model_dir=args.model_dir)
+  #
+  # estimator.train(
+  #   steps=args.steps,
+  #   input_fn=lambda: veri_dataset.get_input_fn(
+  #     is_training=True,
+  #     batch_size=args.batch_size,
+  #     shuffle=True),
+  # )
